7ac84cd9657cf240cf46f645c22eacdccab98ff3
Daniel Kurhinen <35920803+Si1ex@users.noreply.github.com>
2023-05-21 20:25:44 +0300

    eventlistener

--- youtube-downloader/src/App.jsx
+++ youtube-downloader/src/App.jsx
@@ -1,17 +1,24 @@
-import './App.css';
-import sendUrl from "./Script.js";
+import React, { useState } from "react";
 
 function App() {
+  const [url, setUrl] = useState("");
+
+  const handleConvert = () => {
+    console.log(`URL: ${url}`);
+    window.location.href = `http://localhost:4000/download?URL=${url}`;
+  };
+
+  const handleInputChange = (e) => {
+    setUrl(e.target.value);
+  };
+
   return (
     <form>
       <div className="flex flex-col items-center justify-center min-h-screen bg-repeat-space p-10 bg-gradient-to-br from-green-300 via-indigo-300 to-blue-300">
-        <h1 className="text-5xl font-bold text-white text-center ">
+        <h1 className="text-5xl mb-10 font-bold text-white text-center">
           Youtube downloader
         </h1>
-        <h1 className="text-4xl font-bold text-white text-center ">
-          Welcome
-        </h1>
-        <p className="mt-4 text-2xl font-medium text-white mb-7">
+        <p className="m-7 text-2xl font-medium text-white ">
           Input link:
         </p>
         <div className="w-full md:w-1/2 lg:w-1/3 relative flex bg-transparent items-center">
@@ -35,23 +42,21 @@ function App() {
           <input
             type="text"
             name="IP"
-            class="URL-input"
             autoComplete="off"
-            className={`w-full text flex p-3 pl-10 py-2.5 px-4 rounded-full shadow-xl outline-none overflow-y-auto scrollbar-thumb-purple-500 text-gray-900 bg-gray-50 focus:border-purple-400 dark:bg-white`}
+            className="URL-input w-full text flex p-3 pl-10 py-2.5 px-4 rounded-full shadow-xl outline-none overflow-y-auto scrollbar-thumb-purple-500 text-gray-900 bg-gray-50 focus:border-purple-400 dark:bg-white"
             placeholder="Link"
+            value={url}
+            onChange={handleInputChange}
           />
           <div className="w-1/3 md:w-1/4 absolute inset-y-0 right-0 z-30 flex items-center pl-3">
             <button
-              class="convert-button"
-              type="submit"
-              onClick={sendUrl}
-              data-testid="search-button"
-              className="text-white w-full hover:scale-105 ease-in-out duration-100 py-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-400 focus:ring-blue-300 font-medium rounded-full bg-gradient-to-r from-pink-300 to-purple-400 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              type="button"
+              className="convert-button text-white w-full hover:scale-105 ease-in-out duration-100 py-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-400 focus:ring-blue-300 font-medium rounded-full bg-gradient-to-r from-green-300 to-blue-400 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              onClick={handleConvert}
             >
               Convert
             </button>
           </div>
-          
         </div>
       </div>
     </form>
