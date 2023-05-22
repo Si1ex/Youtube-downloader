1979c2208c817ef9162956706db2075f9d0ff29c
Daniel Kurhinen <35920803+Si1ex@users.noreply.github.com>
2023-05-22 18:18:32 +0300

    added zlib compression

--- youtube-downloader/api/download.js
+++ youtube-downloader/api/download.js
@@ -1,18 +1,30 @@
 const ytdl = require('ytdl-core');
+const zlib = require('zlib');
+const stream = require('stream');
+const { promisify } = require('util');
+const pipeline = promisify(stream.pipeline);
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   // Extract the URL and format parameters from the query string
   const { URL, format = 'mp3' } = req.query;
 
   // Set the response headers
   console.log(`Format: ${format}`);
-
   res.setHeader('Content-Disposition', `attachment; filename="file.${format}"`);
+  res.setHeader('Content-Encoding', 'gzip');
 
-  // Download the video from YouTube and pipe it to the response
-  ytdl(URL, {
-    filter: 'audioonly',
-    quality: 'highestaudio',
-    format: format,
-  }).pipe(res);
+  // Create a stream to compress the data using gzip
+  const gzip = zlib.createGzip();
+
+  // Download the video from YouTube, pipe it through the gzip stream to compress it,
+  // and then pipe it to the response
+  await pipeline(
+    ytdl(URL, {
+      filter: 'audioonly',
+      quality: 'highestaudio',
+      format: format,
+    }),
+    gzip,
+    res
+  );
 };
