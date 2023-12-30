9f579ec5aaff107eef1299bf3f1ff659015d1afd
Daniel Kurhinen <35920803+Si1ex@users.noreply.github.com>
2023-12-30 22:47:00 +0200

    .

--- youtube-downloader/server/index.js
+++ youtube-downloader/server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const ytdl = require('ytdl-core');
+const ytdl = require('ytdl-core-discord');
 const app = express();
 
 app.use(cors());
@@ -14,12 +14,20 @@ app.get('/download', async (req, res) => {
     const URL = req.query.URL;
     const format = req.query.format || 'mp3';
 
+    // Logging for debugging
+    console.log('Downloading:', URL, 'in', format, 'format');
+
     const videoInfo = await ytdl.getInfo(URL);
     const title = videoInfo.videoDetails.title;
 
-    res.header('Content-Disposition', `attachment; filename="${title}.${format}"`);
-    res.header('Content-Type', format === 'mp3' ? 'audio/mpeg' : 'video/mp4');
+    // Set Content-Type header
+    const contentType = format === 'mp3' ? 'audio/mpeg' : 'video/mp4';
+    res.header('Content-Type', contentType);
 
+    // Set Content-Disposition header with the title
+    res.header('Content-Disposition', `attachment; filename="${title}.${format}"`);
+
+    // Stream the video
     ytdl(URL, {
       quality: 'highestaudio',
       format: format,
