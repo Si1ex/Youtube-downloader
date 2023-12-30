7f97f85719bb7666317a75e281438dc4c8430be9
Daniel Kurhinen <35920803+Si1ex@users.noreply.github.com>
2023-12-30 22:31:13 +0200

    Testing new code

--- youtube-downloader/server/index.js
+++ youtube-downloader/server/index.js
@@ -9,14 +9,22 @@ app.listen(4000, () => {
   console.log('Server Works at port 4000');
 });
 
-app.get('/download', (req, res) => {
-    var URL = req.query.URL;
-    var format = req.query.format || 'mp3';
+app.get('/download', async (req, res) => {
+  try {
+    const URL = req.query.URL;
+    const format = req.query.format || 'mp3';
+
     res.header('Content-Disposition', `attachment; filename="file.${format}"`);
-  
-    ytdl(URL, {
+
+    // Set the Content-Type based on the format
+    res.header('Content-Type', format === 'mp3' ? 'audio/mpeg' : 'video/mp4');
+
+    await ytdl(URL, {
       quality: 'highestaudio',
       format: format,
     }).pipe(res);
-  });
-  
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
