1be6804d4d16f7fae72333d3e167112c450e263b
Daniel Kurhinen <35920803+Si1ex@users.noreply.github.com>
2023-12-30 22:48:24 +0200

    original code

--- youtube-downloader/server/index.js
+++ youtube-downloader/server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const ytdl = require('ytdl-core-discord');
+const ytdl = require('ytdl-core');
 const app = express();
 
 app.use(cors());
@@ -9,31 +9,14 @@ app.listen(4000, () => {
   console.log('Server Works at port 4000');
 });
 
-app.get('/download', async (req, res) => {
-  try {
-    const URL = req.query.URL;
-    const format = req.query.format || 'mp3';
-
-    // Logging for debugging
-    console.log('Downloading:', URL, 'in', format, 'format');
-
-    const videoInfo = await ytdl.getInfo(URL);
-    const title = videoInfo.videoDetails.title;
-
-    // Set Content-Type header
-    const contentType = format === 'mp3' ? 'audio/mpeg' : 'video/mp4';
-    res.header('Content-Type', contentType);
-
-    // Set Content-Disposition header with the title
-    res.header('Content-Disposition', `attachment; filename="${title}.${format}"`);
-
-    // Stream the video
+app.get('/download', (req, res) => {
+    var URL = req.query.URL;
+    var format = req.query.format || 'mp3';
+    res.header('Content-Disposition', `attachment; filename="file.${format}"`);
+  
     ytdl(URL, {
       quality: 'highestaudio',
       format: format,
     }).pipe(res);
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Internal Server Error');
-  }
-});
+  });
+  
